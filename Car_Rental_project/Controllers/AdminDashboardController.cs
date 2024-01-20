09a0e734f03784bbfb00f58358ce27b5a75422ab
Shiro <Shiro@MSI>
2024-01-20 11:14:35 +0100

    Dodaj pliki projektów.

---
+++ Car_Rental_project/Controllers/AdminDashboardController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Utility;
+
+namespace Car_Rental_project.Controllers
+{
+    [Authorize(Roles = Enums.ConstUserRoles.Admin)]
+    public class AdminDashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+
+    }
+}
---
+++ Car_Rental_project/Controllers/EmployeeDashboardController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Utility;
+
+namespace Car_Rental_project.Controllers
+{
+    [Authorize(Roles = Enums.ConstUserRoles.Employee)]
+    public class EmployeeDashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+
+    }
+}
